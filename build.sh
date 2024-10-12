4884f09e67ee3cc7ba1b86993abfc5f53a9131b8
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2024-10-12 19:39:43 +0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+lb config -d bullseye \
+          --debian-installer live \
+          --debian-installer-distribution bookworm \
+          --debian-installer-gui true \
+          --archive-areas "main contrib non-free" \
+          --debootstrap-options "--variant=minbase" \
+          --iso-publisher 'Sway; https://github.com/hanedotoaranea/sway-main-12/; arachnoideaqinki@gmail.com' \
+          --iso-volume debian-sway-live \
+          --clean \
+          --color \
+          --image-name debian-sway-live-12.beta
+          
+sudo lb build
